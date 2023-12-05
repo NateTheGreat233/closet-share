e04ae61558e8c4fab4ea1e006d31e05f18bf466e
Nathan <nathang0@mit.edu>
2023-12-04 19:12:53 -0500

    first version of borrowed page

---
+++ client/components/ClothingItem/BorrowedItemComponent.vue
@@ -0,0 +1,126 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const { owner, name, description, imageUrl } = defineProps({
+  owner: String,
+  name: String,
+  description: String,
+  imageUrl: String,
+});
+
+const onReturnClick = () => {};
+
+const onContactOwnerClick = () => {};
+
+const onViewContractClick = () => {};
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <div class="image-container">
+        <img :src="imageUrl" />
+      </div>
+      <div class="description-container">
+        <div class="info-column">
+          <h2>{{ name?.toUpperCase() }}</h2>
+          <div class="info-descriptions">
+            <h2>owner: @{{ owner }}</h2>
+            <h2>date borrowed: TODO</h2>
+            <h2>return date: TODO</h2>
+            <h2>notes: {{ description }}</h2>
+          </div>
+        </div>
+        <div class="actions-column">
+          <div class="button-container">
+            <h2 class="button-text">I have returned this item</h2>
+          </div>
+          <div class="button-container">
+            <h2 class="button-text">contact owner</h2>
+          </div>
+          <div class="button-container">
+            <h2 class="button-text">view contract details</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  gap: 40px;
+  width: 100%;
+}
+
+.button-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--gray);
+  padding: 10px;
+  border-radius: 15px;
+  min-width: 300px;
+}
+
+.button-container:hover {
+  cursor: pointer;
+  background-color: var(--gray-hover);
+}
+
+.button-text {
+  user-select: none;
+}
+
+h2 {
+  margin: 0px;
+}
+
+img {
+  object-fit: contain;
+  height: 100%;
+  width: 100%;
+}
+
+.actions-column {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  gap: 20px;
+}
+
+.info-column {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+}
+
+.info-descriptions {
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+}
+
+.description-container {
+  display: flex;
+  flex-grow: 2;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+  background-color: var(--light-gray);
+  padding: 20px;
+}
+
+.image-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--light-gray);
+  padding: 10px;
+  width: 300px;
+  height: 250px;
+}
+</style>
--- client/views/BorrowedView.vue
+++ client/views/BorrowedView.vue
@@ -1,7 +1,105 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Search from "@/components/Search/Search.vue";
+import { onBeforeMount, ref } from "vue";
+import BorrowedItemComponent from "../components/ClothingItem/BorrowedItemComponent.vue";
+import { fetchy } from "../utils/fetchy";
+
+const loaded = ref(false);
+let clothingItems = ref<Array<Record<string, string>>>([]);
+let searchOwner = ref("");
+
+const getClothingItems = async (owner?: string) => {
+  let query: Record<string, string> = owner !== undefined ? { owner } : {};
+  let clothingItemResults;
+  try {
+    clothingItemResults = await fetchy("/api/clothingItems", "GET", { query });
+  } catch (_) {
+    return;
+  }
+  searchOwner.value = owner ? owner : "";
+  clothingItems.value = clothingItemResults;
+};
+
+onBeforeMount(async () => {
+  await getClothingItems();
+  loaded.value = true;
+});
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <div class="container">
+      <Search />
+      <div class="top-row">
+        <h1>currently borrowing</h1>
+        <div class="history-button-container">
+          <img src="@/assets/images/book.png" />
+          <h2>HISTORY</h2>
+        </div>
+      </div>
+      <div class="listing-wrapper">
+        <div v-for="item in clothingItems" class="listing-container">
+          <BorrowedItemComponent :owner="item.owner" :name="item.name" :description="item.description" :imageUrl="item.imageUrl" />
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  padding-left: 100px;
+  padding-right: 100px;
+}
+
+.history-button-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  padding: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  height: 30px;
+  background-color: var(--gray);
+  border-radius: 15px;
+}
+
+.history-button-container:hover {
+  cursor: pointer;
+  background-color: var(--gray-hover);
+}
+
+img {
+  height: 20px;
+}
+
+.listing-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.listing-wrapper {
+  gap: 40px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.top-row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+  user-select: none;
+}
+</style>
